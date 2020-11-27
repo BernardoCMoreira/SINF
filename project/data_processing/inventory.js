cc8f8e662391d681e8647e37725785e626e5f38b
Tomas Figueiredo <tomasviana_cof@hotmail.com>
2020-11-27 20:50:02 +0000

    Created working file for data process hooks

--- project/data_processing/inventory.js
+++ project/data_processing/inventory.js
@@ -1,9 +1,13 @@
-const axios = require('axios');
+const axios = require("axios");
 
+const f1 = () => {
+  axios
+    .get(`http://localhost:${process.env.PORT}/api/inventory/products`)
+    .then((resp) => {
+      console.log(resp.data);
+    });
+};
 
-const f = axios.get(`http://localhost:${process.env.PORT}/api/inventory/products`).then(resp => {
-    console.log(resp.data);
-})
-
-
-module.exports.f = f;
+module.exports = {
+  function1: f1,
+};
