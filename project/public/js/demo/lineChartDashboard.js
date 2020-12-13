eb0649e1cc31d25420da43c9b2e832b4ad22c974
Claudia <up201604832@fe.up.pt>
2020-12-13 17:28:19 +0000

    Finished dashboard

--- project/public/js/demo/lineChartDashboard.js
+++ project/public/js/demo/lineChartDashboard.js
@@ -3,17 +3,18 @@ var ctxL = document.getElementById("lineChartDashboard");
 var arrays = ctxL.dataset.salespurchases.split(",");
 var graph1 = [];
 var graph2 = [];
+
 for (let i = 0; i < arrays.length / 2; i++) {
-    graph1.push(parseFloat(arrays[i]));
+    graph1.push(parseFloat(arrays[i]).toFixed(2));
 }
 for (let j = arrays.length / 2; j < arrays.length; j++) {
-    graph2.push(parseFloat(arrays[j]));
+    graph2.push(parseFloat(arrays[j]).toFixed(2));
 }
 
 var myLineChart = new Chart(ctxL, {
     type: 'line',
     data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
         datasets: [{
                 label: "Sales",
                 data: graph1,
@@ -39,6 +40,41 @@ var myLineChart = new Chart(ctxL, {
         ]
     },
     options: {
-        responsive: true
+        responsive: true,
+        scales: {
+            xAxes: [
+                {
+                    time: {
+                        unit: 'date'
+                    },
+                    gridLines: {
+                        display: false,
+                        drawBorder: false
+                    },
+                    ticks: {
+                        maxTicksLimit: 7
+                    }
+                }
+            ],
+            yAxes: [
+                {
+                    ticks: {
+                        maxTicksLimit: 5,
+                        padding: 10,
+                        // Include a dollar sign in the ticks
+                        callback: function(value, index, values) {
+                            return '€' + number_format(value);
+                        }
+                    },
+                    gridLines: {
+                        color: "rgb(234, 236, 244)",
+                        zeroLineColor: "rgb(234, 236, 244)",
+                        drawBorder: false,
+                        borderDash: [2],
+                        zeroLineBorderDash: [2]
+                    }
+                }
+            ],
+        },
     }
 });
